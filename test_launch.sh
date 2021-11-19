4f9ac809b2826308bb8668989dc219b936a8496e
florencepascual <florence.pascual@ibm.com>
2021-11-19 07:45:15 -0800

    rm dockerd-strating.sh

--- test_launch.sh
+++ test_launch.sh
@@ -2,8 +2,37 @@
 
 set -ue
 
-# Start the dockerd and wait for it to start
-source /usr/local/bin/dockerd-starting.sh
+# Start the docker daemon in the background
+bash /usr/local/bin/dockerd-entrypoint.sh &
+
+# Check if the dockerd has started
+TIMEOUT=10
+DAEMON="dockerd"
+i=0
+echo $DAEMON
+while [ $i -lt $TIMEOUT ] && ! /usr/bin/pgrep $DAEMON
+do
+    i=$((i+1))
+    sleep 2
+done
+
+pid=`/usr/bin/pgrep $DAEMON`
+
+if [ -z "$pid" ]
+then
+    echo "$DAEMON has not started after $(($TIMEOUT*2)) seconds"
+    exit 1
+else
+    echo "Found $DAEMON pid:$pid"
+    if [[ ! -z ${DOCKER_SECRET_AUTH+z} ]] && [ ! -d /root/.docker ]
+    then
+        mkdir /root/.docker
+        echo "$DOCKER_SECRET_AUTH" > /root/.docker/config.json
+        echo "Docker login"
+    fi
+    echo "Launching docker info"
+    docker info
+fi
 
 ctr -v
 
