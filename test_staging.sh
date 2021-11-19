f04bb50b1743fec2be723d776b8d91edea0dcc1a
florencepascual <florence.pascual@ibm.com>
2021-11-19 04:35:45 -0800

    Env files

--- test_staging.sh
+++ test_staging.sh
@@ -36,8 +36,12 @@ do
 
         echo "### ### Running the tests from the container: ${CONT_NAME} ### ###" 2>&1 | tee -a ${LOG}
         docker run -d -v /workspace:/workspace --env DOCKER_SECRET_AUTH --env DISTRO_NAME --env DISTRO_VERS --env LOG --privileged --name ${CONT_NAME} ${IMAGE_NAME}
-        docker logs $CONT_NAME 2>&1 | tee ${TEST_LOG}
 
+        status_code="$(docker container wait $CONT_NAME)"
+
+        if [[ ${status_code} -ne 0 ]]; then
+            docker logs $CONT_NAME 2>&1 | tee ${TEST_LOG}
+        fi
         echo "### ### # Cleanup: ${CONT_NAME} # ### ###"
         docker stop ${CONT_NAME}
         docker rm ${CONT_NAME}
