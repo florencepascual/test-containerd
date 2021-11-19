0ca861c20d9a2382918c2fbb020d13564636f67f
florencepascual <florence.pascual@ibm.com>
2021-11-19 04:19:03 -0800

    test_staging.sh

--- test_staging.sh
+++ test_staging.sh
@@ -24,32 +24,19 @@ do
         IMAGE_NAME="t_docker_${DISTRO_NAME}_${DISTRO_VERS}"
         CONT_NAME="t_docker_run_${DISTRO_NAME}_${DISTRO_VERS}"
         BUILD_LOG="build_${DISTRO_NAME}_${DISTRO_VERS}.log"
+        TEST_LOG="test_${DISTRO_NAME}_${DISTRO_VERS}.log"
 
         export DISTRO_NAME
         export DISTRO_VERS
 
-        echo "### # Copying the packages and the dockerfile for ${DISTRO} # ###" 2>&1 | tee -a ${LOG}
+        pushd ${PATH_DOCKERFILE}-${PACKTYPE}
         # Building the test image
         echo "### ## Building the test image: ${IMAGE_NAME} ## ###" 2>&1 | tee -a ${LOG}
-        docker build -t ${IMAGE_NAME} --build-arg DISTRO_NAME=${DISTRO_NAME} --build-arg DISTRO_VERS=${DISTRO_VERS} --build-arg DOCKER_VERS --build-arg CONTAINERD_VERS . 2>&1 | tee ${DIR_TEST}/${BUILD_LOG}
-
-        if [[ $? -ne 0 ]]; then
-            echo "ERROR: docker build failed for ${DISTRO}, see details from '${BUILD_LOG}'" 2>&1 | tee -a ${LOG}
-            continue
-        else
-            echo "Docker build for ${DISTRO} done" 2>&1 | tee -a ${LOG}
-        fi
+        docker build -t ${IMAGE_NAME} --build-arg DISTRO_NAME=${DISTRO_NAME} --build-arg DISTRO_VERS=${DISTRO_VERS} --build-arg DOCKER_VERS=${DOCKER_VERS} --build-arg CONTAINERD_VERS=${CONTAINERD_VERS} . 2>&1 | tee ${BUILD_LOG}
 
         echo "### ### Running the tests from the container: ${CONT_NAME} ### ###" 2>&1 | tee -a ${LOG}
-        docker run -d -v /workspace:/workspace -v ${PATH_SCRIPTS}:${PATH_SCRIPTS} --env DOCKER_SECRET_AUTH --env DISTRO_NAME --env DISTRO_VERS --env PATH_SCRIPTS --env DIR_TEST --env LOG --privileged --name ${CONT_NAME} ${IMAGE_NAME}
-        status_code="$(docker container wait $CONT_NAME)"
-        if [[ ${status_code} -ne 0 ]]; then
-            echo "ERROR: The test suite failed for ${DISTRO}. See details from '${TEST_LOG}'" 2>&1 | tee -a ${LOG}
-            docker logs $CONT_NAME 2>&1 | tee ${DIR_TEST}/${TEST_LOG}
-        else
-            docker logs $CONT_NAME 2>&1 | tee ${DIR_TEST}/${TEST_LOG}
-            echo "Tests done" 2>&1 | tee -a ${LOG}
-        fi
+        docker run -d -v /workspace:/workspace --env DOCKER_SECRET_AUTH --env DISTRO_NAME --env DISTRO_VERS --env LOG --privileged --name ${CONT_NAME} ${IMAGE_NAME}
+        docker logs $CONT_NAME 2>&1 | tee ${TEST_LOG}
 
         echo "### ### # Cleanup: ${CONT_NAME} # ### ###"
         docker stop ${CONT_NAME}
